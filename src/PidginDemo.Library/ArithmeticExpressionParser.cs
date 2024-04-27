55e368bc8da6c8039ab5c0d994be73c853307f7b
CodeFuller <CodeFuller@gmail.com>
2024-04-27 08:21:58 +0200

    Simplify definition for token parsers

--- src/PidginDemo.Library/ArithmeticExpressionParser.cs
+++ src/PidginDemo.Library/ArithmeticExpressionParser.cs
@@ -8,20 +8,23 @@ namespace PidginDemo.Library
 {
 	public static class ArithmeticExpressionParser
 	{
+		private static readonly Parser<char, char> OpenParenthesisParser = TokenParser('(');
+		private static readonly Parser<char, char> CloseParenthesisParser = TokenParser(')');
+
 		private static readonly Parser<char, ArithmeticExpression> NumberParser = Map<char, double, ArithmeticExpression>(value => new NumberExpression(value), TokenParser(Real));
 
 		// TODO: Can these parsers definitions be simplified?
 		private static readonly Parser<char, Func<ArithmeticExpression, ArithmeticExpression, ArithmeticExpression>> AdditionParser =
-			TokenParser(Char('+')).Select<Func<ArithmeticExpression, ArithmeticExpression, ArithmeticExpression>>(_ => (addend1, addend2) => new AdditionExpression(addend1, addend2));
+			TokenParser('+').Select<Func<ArithmeticExpression, ArithmeticExpression, ArithmeticExpression>>(_ => (addend1, addend2) => new AdditionExpression(addend1, addend2));
 
 		private static readonly Parser<char, Func<ArithmeticExpression, ArithmeticExpression, ArithmeticExpression>> SubtractionParser =
-			TokenParser(Char('-')).Select<Func<ArithmeticExpression, ArithmeticExpression, ArithmeticExpression>>(_ => (minuend, subtrahend) => new SubtractionExpression(minuend, subtrahend));
+			TokenParser('-').Select<Func<ArithmeticExpression, ArithmeticExpression, ArithmeticExpression>>(_ => (minuend, subtrahend) => new SubtractionExpression(minuend, subtrahend));
 
 		private static readonly Parser<char, Func<ArithmeticExpression, ArithmeticExpression, ArithmeticExpression>> MultiplicationParser =
-			TokenParser(Char('*')).Select<Func<ArithmeticExpression, ArithmeticExpression, ArithmeticExpression>>(_ => (multiplier, multiplicand) => new MultiplicationExpression(multiplier, multiplicand));
+			TokenParser('*').Select<Func<ArithmeticExpression, ArithmeticExpression, ArithmeticExpression>>(_ => (multiplier, multiplicand) => new MultiplicationExpression(multiplier, multiplicand));
 
 		private static readonly Parser<char, Func<ArithmeticExpression, ArithmeticExpression, ArithmeticExpression>> DivisionParser =
-			TokenParser(Char('/')).Select<Func<ArithmeticExpression, ArithmeticExpression, ArithmeticExpression>>(_ => (dividend, divisor) => new DivisionExpression(dividend, divisor));
+			TokenParser('/').Select<Func<ArithmeticExpression, ArithmeticExpression, ArithmeticExpression>>(_ => (dividend, divisor) => new DivisionExpression(dividend, divisor));
 
 		private static readonly Parser<char, ArithmeticExpression> ExpressionParser =
 			Pidgin.Expression.ExpressionParser.Build<char, ArithmeticExpression>(
@@ -39,20 +42,19 @@ namespace PidginDemo.Library
 			return parseResult.Success ? parseResult.Value : null;
 		}
 
-		// TODO: Is there simpler way for skipping whitespaces?
+		private static Parser<char, char> TokenParser(char token)
+		{
+			return Char(token).Before(SkipWhitespaces);
+		}
+
 		private static Parser<char, T> TokenParser<T>(Parser<char, T> token)
 		{
 			return Try(token).Before(SkipWhitespaces);
 		}
 
-		private static Parser<char, string> TokenParser(string token)
-		{
-			return TokenParser(String(token));
-		}
-
 		private static Parser<char, T> ParenthesizedParser<T>(Parser<char, T> parser)
 		{
-			return parser.Between(TokenParser("("), TokenParser(")"));
+			return parser.Between(OpenParenthesisParser, CloseParenthesisParser);
 		}
 	}
 }
